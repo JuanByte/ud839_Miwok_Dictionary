900676a944b3380378bea73eb6864340eb358579
Juan <gjuans.pd@gmail.com>
2018-09-27 16:04:03 -0400

    Add child TextViews with number words

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -14,7 +14,7 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-
+        //Create list of words
         ArrayList<String> words = new ArrayList<>();
         words.add("one");
         words.add("two");
@@ -27,5 +27,21 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
+        //find the root view so we can add child views to it and assign it to
+        // rootView linear layout.
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        //create a variable to keep track of the current index position.
+        int index = 0;
+        //keep looping until we reach the end of the list
+        while (index < words.size()) {
+            //create a new text view
+            TextView wordView = new TextView(this);
+            //set text to be word at the current position
+            wordView.setText(words.get(index));
+            //add this text view as another child to the root view of this layout
+            rootView.addView(wordView);
+            //increment index by 1
+            index++;
+        }
     }
 }

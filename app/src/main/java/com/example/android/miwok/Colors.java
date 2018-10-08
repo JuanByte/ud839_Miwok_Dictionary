36e1539d2a7bbd8173ab7285b317d98c14b66f2f
Juan <gjuans.pd@gmail.com>
2018-10-08 13:24:05 -0400

    Add list of colors on Colors Activity

--- app/src/main/java/com/example/android/miwok/Colors.java
+++ app/src/main/java/com/example/android/miwok/Colors.java
@@ -2,6 +2,13 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Colors extends AppCompatActivity {
 
@@ -9,6 +16,33 @@ public class Colors extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        //Create list of words
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+        //Create an {@link ArrayAdapter}, whose data source is a list of String.
+        //adapter knows how to create layouts for each item in this list, using the
+        //simple_list_item_1.xml layout resource defined in the Android framework.
+        //This list item layout contains a single{@link TextView}, which the adapter will
+        //set to display a single word.
+        WordAdapter itemsAdapter = new WordAdapter(this , words);
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.colors_list);
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
     }
+
 }
 

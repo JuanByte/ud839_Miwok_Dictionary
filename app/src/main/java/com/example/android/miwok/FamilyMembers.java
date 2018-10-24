0529d29b3e6e4b4335ee9ace56551a071bba9f8a
Juan <gjuans.pd@gmail.com>
2018-10-24 15:47:39 -0400

    Clean up MediaPlayer resources

--- app/src/main/java/com/example/android/miwok/FamilyMembers.java
+++ app/src/main/java/com/example/android/miwok/FamilyMembers.java
@@ -16,6 +16,16 @@ public class FamilyMembers extends AppCompatActivity {
     //Handle all audio play
     MediaPlayer mediaPlayer;
 
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,13 +62,34 @@ public class FamilyMembers extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Release the media player if it currently exists because we are about to
+                // play a different sound file
+                releaseMediaPlayer();
                 // Get the current word position
                 Word word = words.get(position);
                 // Create a media player object with the corresponding audio file
                 mediaPlayer = MediaPlayer.create(FamilyMembers.this, word.getmAudioResourceId());
                 // Play the audio for the corresponding word
                 mediaPlayer.start();
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mediaPlayer.setOnCompletionListener(onCompletionListener);
             }
         });
+    }
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -12,7 +12,17 @@ import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
     //Handle all audio play
-    MediaPlayer mediaPlayer;
+    private MediaPlayer mediaPlayer;
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +60,9 @@ public class NumbersActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Release the media player if it currently exists because we are about to
+                // play a different sound file
+                releaseMediaPlayer();
                 // Get the current word position
                 Word word = words.get(position);
                 Log.v("NumbersActivity", "Current Word state is: " + word.toString());
@@ -57,7 +70,27 @@ public class NumbersActivity extends AppCompatActivity {
                 mediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getmAudioResourceId());
                 // Play the audio for the corresponding word
                 mediaPlayer.start();
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mediaPlayer.setOnCompletionListener(onCompletionListener);
             }
         });
+
     }
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
+    }
+
 }

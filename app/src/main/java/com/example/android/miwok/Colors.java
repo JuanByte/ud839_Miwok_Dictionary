716d177bba556e3d9524801b854206b9f7a4aa9d
Juan <gjuans.pd@gmail.com>
2018-11-10 18:45:31 -0300

    Add Colors Fragment

--- app/src/main/java/com/example/android/miwok/Colors.java
+++ app/src/main/java/com/example/android/miwok/Colors.java
@@ -13,125 +13,14 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class Colors extends AppCompatActivity {
-    //Handle all audio play
-    MediaPlayer mediaPlayer;
-    /**
-     * This listener gets triggered when the {@link MediaPlayer} has completed
-     * playing the audio file.
-     */
-    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            releaseMediaPlayer();
-        }
-    };
-    // Handle AudioManager
-    AudioManager mAudioManager;
-    // Audio manager listener
-    AudioManager.OnAudioFocusChangeListener audioFocusChangeListener= new AudioManager.OnAudioFocusChangeListener() {
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-            switch (focusChange){
-                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                    // we pause our audio and set it from the beginning
-                    mediaPlayer.pause();
-                    mediaPlayer.seekTo(0);
-                    break;
-                case AudioManager.AUDIOFOCUS_LOSS:
-                    // release all media player resources and audio focus
-                    releaseMediaPlayer();
-                    break;
-                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                    mediaPlayer.pause();
-                    mediaPlayer.seekTo(0);
-                    break;
-                case AudioManager.AUDIOFOCUS_GAIN:
-                    mediaPlayer.start();
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-        // Create an instance of AudioManager to manage audio focus
-        mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
-        //Create list of words
-        final ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
-        words.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
-        words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
-        words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
-        words.add(new Word("black", "kululli", R.drawable.color_black, R.raw.color_black));
-        words.add(new Word("white", "kelelli", R.drawable.color_white, R.raw.color_white));
-        words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
-        words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
-        //Create an {@link ArrayAdapter}, whose data source is a list of String.
-        //adapter knows how to create layouts for each item in this list, using the
-        //simple_list_item_1.xml layout resource defined in the Android framework.
-        //This list item layout contains a single{@link TextView}, which the adapter will
-        //set to display a single word.
-        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_colors);
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml file.
-        ListView listView = (ListView) findViewById(R.id.list);
-        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
-        // {@link ListView} will display list items for each word in the list of words.
-        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
-        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        listView.setAdapter(itemsAdapter);
-        //Set audio for each listview throw the method setOnClickListener, which takes object of OnclickItemListener
-        //as a parameter and then override the method onItemClick Listener to play the audio.
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Release the media player if it currently exists because we are about to
-                // play a different sound file
-                releaseMediaPlayer();
-                // Get the current word position
-                Word word = words.get(position);
-                //Request audio Focus for playback
-                final int aFrequest = mAudioManager.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-                if (aFrequest == AudioManager.AUDIOFOCUS_REQUEST_GRANTED){
-                    // Create a media player object with the corresponding audio file
-                    mediaPlayer = MediaPlayer.create(Colors.this, word.getmAudioResourceId());
-                    // Play the audio for the corresponding word
-                    mediaPlayer.start();
-                    // Setup a listener on the media player, so that we can stop and release the
-                    // media player once the sound has finished playing.
-                    mediaPlayer.setOnCompletionListener(onCompletionListener);}
-            }
-        });
-    }
-    /**
-     * Clean up the media player by releasing its resources.
-     */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mediaPlayer.release();
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mediaPlayer = null;
-            // Release audio focus regardless of whether or not we were granted audio focus. This
-            //also unregister the AudioFocusChangeListener so we don't get any more callbacks.
-            mAudioManager.abandonAudioFocus(audioFocusChangeListener);
-        }
-    }
-
-    /**
-     * Realise media resources player whenever the user leaves the activity
-     */
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.v("MainActivity", "Activity has Stopped");
-        releaseMediaPlayer();
+        setContentView(R.layout.activity_category);
+        //relate Colors fragment with activity_category layout and this Color Activity as its
+        //container
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ColorsFragment()).commit();
     }
 
 }
